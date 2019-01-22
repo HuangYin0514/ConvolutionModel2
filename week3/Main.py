f693aafc20f618892992f4022afcf6ea3082b0ed
huangyin0611 <1071324110@qq.com>
2019-01-22 14:09:43 +0800

    initialization commit

--- week3/Main.py
+++ week3/Main.py
@@ -5,21 +5,7 @@
 # @Software : PyCharm
 
 
-import argparse
-import os
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import imshow
-import scipy.io
-import scipy.misc
-import numpy as np
-import pandas as pd
-import PIL
-import tensorflow as tf
-from keras import backend as K
-from keras.layers import Input, Lambda, Conv2D
-from keras.models import load_model, Model
-from yolo_utils import read_classes, read_anchors, generate_colors, preprocess_image, draw_boxes, scale_boxes
-from yad2k.models.keras_yolo import yolo_head, yolo_boxes_to_corners, preprocess_true_boxes, yolo_loss, yolo_body
+from week3.yad2k.models.keras_yolo import yolo_head
 from week3.MyMethod import *
 
 if __name__ == '__main__':
---
+++ week3/yad2k/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2019/1/22 14:09
+# @Author   : HuangYin
+# @FileName : __init__.py.py
+# @Software : PyCharm
