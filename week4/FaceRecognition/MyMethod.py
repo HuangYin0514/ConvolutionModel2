df2feb4ea9e38321676f256917f9b0543a383fcc
huangyin0611 <1071324110@qq.com>
2019-01-24 16:04:59 +0800

    triplet_loss

--- week4/FaceRecognition/MyMethod.py
+++ week4/FaceRecognition/MyMethod.py
@@ -3,3 +3,47 @@
 # @Author   : HuangYin
 # @FileName : MyMethod.py
 # @Software : PyCharm
+
+
+from keras.models import Sequential
+from keras.layers import Conv2D, ZeroPadding2D, Activation, Input, concatenate
+from keras.models import Model
+from keras.layers.normalization import BatchNormalization
+from keras.layers.pooling import MaxPooling2D, AveragePooling2D
+from keras.layers.merge import Concatenate
+from keras.layers.core import Lambda, Flatten, Dense
+from keras.initializers import glorot_uniform
+from keras.engine.topology import Layer
+from keras import backend as K
+
+K.set_image_data_format('channels_first')
+import cv2
+import os
+import numpy as np
+from numpy import genfromtxt
+import pandas as pd
+import tensorflow as tf
+from fr_utils import *
+from inception_blocks_v2 import *
+
+
+def triplet_loss(y_true, y_pred, alpha=0.2):
+    """
+        Implementation of the triplet loss as defined by formula (3)
+
+        Arguments:
+        y_true -- true labels, required when you define a loss in Keras, you don't need it in this function.
+        y_pred -- python list containing three objects:
+                anchor -- the encodings for the anchor images, of shape (None, 128)
+                positive -- the encodings for the positive images, of shape (None, 128)
+                negative -- the encodings for the negative images, of shape (None, 128)
+
+        Returns:
+        loss -- real number, value of the loss
+        """
+    anchor, positive, negative = y_pred[0], y_pred[1], y_pred[2]
+    pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, positive)))
+    neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, negative)))
+    basic_loss = tf.add(tf.subtract(pos_dist, neg_dist), alpha)
+    loss = tf.maximum(tf.reduce_mean(basic_loss), 0.0)
+    return loss
--- week4/FaceRecognition/main.py
+++ week4/FaceRecognition/main.py
@@ -15,6 +15,7 @@ from keras.layers.core import Lambda, Flatten, Dense
 from keras.initializers import glorot_uniform
 from keras.engine.topology import Layer
 from keras import backend as K
+
 K.set_image_data_format('channels_first')
 import cv2
 import os
@@ -24,3 +25,16 @@ import pandas as pd
 import tensorflow as tf
 from fr_utils import *
 from inception_blocks_v2 import *
+from week4.FaceRecognition.MyMethod import *
+
+if __name__ == '__main__':
+    with tf.Session() as test:
+        tf.set_random_seed(1)
+        y_true = (None, None, None)
+        y_pred = (
+            tf.random_normal([3, 128], mean=6, stddev=0.1, seed=1),
+            tf.random_normal([3, 128], mean=1, stddev=1, seed=1),
+            tf.random_normal([3, 128], mean=3, stddev=4, seed=1),
+        )
+        loss = triplet_loss(y_true, y_pred)
+        print("loss = ", str(loss.eval()))
