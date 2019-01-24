c5bdfcb18511c8f3896f0044a966243fd57ea6d5
huangyin0611 <1071324110@qq.com>
2019-01-24 17:11:54 +0800

    verify

--- week4/FaceRecognition/MyMethod.py
+++ week4/FaceRecognition/MyMethod.py
@@ -47,3 +47,28 @@ def triplet_loss(y_true, y_pred, alpha=0.2):
     basic_loss = tf.add(tf.subtract(pos_dist, neg_dist), alpha)
     loss = tf.maximum(tf.reduce_mean(basic_loss), 0.0)
     return loss
+
+
+def verify(image_path, identity, database, mdoel):
+    """
+        Function that verifies if the person on the "image_path" image is "identity".
+
+        Arguments:
+        image_path -- path to an image
+        identity -- string, name of the person you'd like to verify the identity. Has to be a resident of the Happy house.
+        database -- python dictionary mapping names of allowed people's names (strings) to their encodings (vectors).
+        model -- your Inception model instance in Keras
+
+        Returns:
+        dist -- distance between the image_path and the image of "identity" in the database.
+        door_open -- True, if the door should open. False otherwise.
+        """
+    encoding = img_to_encoding(image_path, Model)
+    dist = np.linalg.norm(encoding - database[identity])
+    if dist < 0.7:
+        print("It`s " + str(identity) + ", welcome home!")
+        door_open = True
+    else:
+        print("It's not " + str(identity) + ", please go away")
+        door_open = False
+    return dist, door_open
