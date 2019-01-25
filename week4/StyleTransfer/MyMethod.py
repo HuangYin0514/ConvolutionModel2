e5d8aa34f115082a1c545d82417b272137b57aa9
huangyin0611 <1071324110@qq.com>
2019-01-25 15:41:39 +0800

    compute_content_cost

--- week4/StyleTransfer/MyMethod.py
+++ week4/StyleTransfer/MyMethod.py
@@ -14,3 +14,20 @@ from nst_utils import *
 import numpy as np
 import tensorflow as tf
 
+
+def compute_content_cost(a_C, a_G):
+    """
+     Computes the content cost
+
+     Arguments:
+     a_C -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing content of the image C
+     a_G -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing content of the image G
+
+     Returns:
+     J_content -- scalar that you compute using equation 1 above.
+     """
+    m, n_H, n_W, n_C = a_G.get_shape().as_list()
+    a_C_unrolled = tf.transpose(a_C)
+    a_G_unrolled = tf.transpose(a_G)
+    J_content = (1 / (4 * n_H * n_W * n_C)) * tf.reduce_sum(tf.pow((a_G_unrolled - a_C_unrolled), 2))
+    return J_content
